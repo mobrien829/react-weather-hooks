b9b59661a23deb196bce676d478d93902609af8f
Michael <mobrien@colgate.edu>
2020-04-22 19:27:07 -0400

    basic fetch working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [location, setLocation] = useState();
+  const [lastForecast, setLastForecast] = useState();
+  const fetchWeather = (event) => {
+    event.preventDefault();
+
+    fetch(
+      `https://still-depths-86857.herokuapp.com/forecast?location=${location}`
+    )
+      .then((res) => res.json())
+      .then((data) => setLastForecast(data));
+  };
+  useEffect(() => {
+    if (lastForecast) {
+      console.log(lastForecast);
+    }
+  });
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +35,20 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        <form onSubmit={(event) => fetchWeather(event)}>
+          <label>
+            Location
+            <input
+              type="text"
+              placeholder="Location"
+              name="location"
+              onChange={(event) => setLocation(event.target.value)}
+            />
+          </label>
+          <input type="submit" value="Get Weather" />
+        </form>
+      </div>
     </div>
   );
 }
